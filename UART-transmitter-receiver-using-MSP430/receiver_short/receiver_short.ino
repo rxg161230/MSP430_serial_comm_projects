741b3907878e8db7d899927e0d88dbfae53d12a4
rxg161230 <43195608+rxg161230@users.noreply.github.com>
2018-09-12 03:14:14 -0500

    Add files via upload

---
+++ UART-transmitter-receiver-using-MSP430/receiver_short/receiver_short.ino
@@ -0,0 +1,94 @@
+//#define led 13
+#include <LiquidCrystal.h>
+const int ledPin =  GREEN_LED;
+const int ledPin1 = RED_LED;
+int val;
+int state;
+LiquidCrystal lcd(P2_0, P2_1, P2_2, P2_3, P2_4, P2_5);
+
+void setup() {
+Serial.begin(9600);
+lcd.begin(16, 2);
+lcd.clear();
+lcd.print("Hi EE/CE1202");
+pinMode(ledPin, OUTPUT);
+pinMode(ledPin1, OUTPUT);
+digitalWrite(ledPin, LOW);
+digitalWrite(ledPin1, LOW);
+  
+  
+}
+
+void loop() {
+if (Serial.available() > 0)
+{
+val = Serial.read();
+
+if (val == 1 && state ==0)
+{
+digitalWrite(ledPin, HIGH);
+digitalWrite(ledPin1,LOW);
+//lcd.setCursor(0,1);
+lcd.clear();
+lcd.print("GREEN LED ON");
+state = 1;
+//delay(150);
+}
+else if (val == 1 && state == 1)
+
+{
+digitalWrite(ledPin,LOW);
+digitalWrite(ledPin1,LOW);
+//lcd.setCursor(0,1);
+//lcd.clear();
+//lcd.print("G");
+
+state = 0;
+//delay(250);
+}
+else if (val == 2 && state ==0)
+{
+digitalWrite(ledPin1, HIGH);
+digitalWrite(ledPin,LOW);
+//lcd.setCursor(0,1);
+lcd.clear();
+lcd.print("RED LED ON");
+state = 1;
+//delay(150);
+}
+else if (val == 2 && state == 1)
+
+{
+digitalWrite(ledPin1,LOW);
+digitalWrite(ledPin,LOW);
+//lcd.setCursor(0,1);
+//lcd.clear();
+//lcd.print("G");
+state = 0;
+//delay(250);
+}
+
+else if (val == 3 && state ==0)
+{
+digitalWrite(ledPin1, HIGH);
+digitalWrite(ledPin, HIGH);
+//lcd.setCursor(0,1);
+lcd.clear();
+lcd.print("BOTH LEDs ON");
+state = 1;
+//delay(150);
+}
+else if (val == 3 && state == 1)
+{
+digitalWrite(ledPin,LOW);
+digitalWrite(ledPin1,LOW);
+//lcd.setCursor(0,1);
+//lcd.clear();
+//lcd.print("B");
+state = 0;
+//delay(250);
+}
+
+
+}
+} 
---
+++ UART-transmitter-receiver-using-MSP430/transmitter_short/transmitter_short_now.ino
@@ -0,0 +1,59 @@
+//#define button 13
+int buttonState = 0;
+int buttonState1= 0;
+int buttonState2= 0;
+//const int ledPin =  GREEN_LED;
+//const int ledPin1 = RED_LED;
+void setup() {
+Serial.begin(9600);
+pinMode(10, INPUT_PULLUP);
+pinMode(9, INPUT_PULLUP);
+pinMode(8, INPUT_PULLUP);
+//pinMode(ledPin, OUTPUT);
+//pinMode(ledPin1, OUTPUT);
+//digitalWrite(ledPin, LOW);
+//digitalWrite(ledPin1, LOW);
+
+}
+
+void loop() {
+buttonState = digitalRead(10);
+buttonState1 = digitalRead(9);
+buttonState2 = digitalRead(8);
+if (buttonState == HIGH)
+{
+Serial.write(1);
+//digitalWrite(ledPin, HIGH);
+//digitalWrite(ledPin1,LOW);
+//delay(500);
+}
+//delay(100);
+
+else 
+{
+}
+if (buttonState1 == HIGH)
+{
+  Serial.write(2);
+ // digitalWrite(ledPin1, HIGH);
+ // digitalWrite(ledPin,LOW);
+  //delay(500);
+}
+//delay(100);
+else 
+{
+}
+if (buttonState2 == HIGH)
+{
+  Serial.write(3);
+ // digitalWrite(ledPin1, HIGH);
+ // digitalWrite(ledPin, HIGH);
+  //delay(500);
+
+}
+}
+  
+
+
+
+
